64dec8b5f02ca6b0154617c6e776ba14ae7b0c76
congibab <49512074+congibab@users.noreply.github.com>
2019-11-12 22:01:44 +0900

    191112 DOTween

---
+++ dotween/Assets/Script/UseEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class UseEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] MovePoint;
+    [SerializeField]
+    private GameObject[] Enemy;
+
+    [Range(0, 5)]
+    public float Speed = 2.0f;
+
+    int endPoint = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.transform.DOMove(MovePoint[endPoint].transform.position, Speed, false).OnComplete(() => NextMove());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void NextMove()
+    {
+        endPoint++;
+        endPoint %=   MovePoint.Length;
+        this.transform.DOMove(MovePoint[endPoint].transform.position, Speed, false).OnComplete(() => NextMove());
+    }
+}
---
+++ dotween/Assets/Script/usePlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class usePlayer : MonoBehaviour
+{
+    public Collider[] hitColliders;
+    private bool CheckOverlap;
+
+    [Range(0, 10)]
+    public float Radius;
+    [Range(0, 5)]
+    public float Speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObjectDetect();
+        FollowMove();
+    }
+
+    private void ObjectDetect()
+    {
+        hitColliders = Physics.OverlapSphere(transform.position, Radius, 1 << 9);
+        CheckOverlap = Physics.CheckSphere(transform.position, Radius, 1 << 9);
+    }
+
+    private void FollowMove()
+    {
+        if (CheckOverlap)
+        {
+            this.transform.DOMove(hitColliders[0].transform.position, Speed, false);
+        }
+
+        else
+        {
+            this.transform.DOMove(transform.position, 0, false);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+}
